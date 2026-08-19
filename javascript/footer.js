9dca7a068657e60899e0a039e014c2caf8d009ac
Marnix <96261289+Marnix47@users.noreply.github.com>
2026-08-19 08:59:33 +0000

    change landing page

--- javascript/footer.js
+++
@@ -1,6 +0,0 @@
-var root = document.documentElement
-window.onload = function(){
-	//console.log(root)
-	root.style.setProperty("--accent-colort", localStorage.getItem("accent"))
-	//console.log(getComputedStyle(root).getPropertyValue("--accent-colort"))
-}
