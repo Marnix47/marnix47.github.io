ebb197c54cf0109c26efa21c182422e413bc1925
Marnix <96261289+Marnix47@users.noreply.github.com>
2026-08-19 07:28:04 +0000

    add: more stations

--- trains/trains/src/StationSelector.js
+++ trains/trains/src/StationSelector.js
@@ -4,9 +4,20 @@ import "./StationSelector.css";
 export default function StationSelector({onChange}) {
     const [selected, setSelected] = useState("NWK");
     const stations = [
-        {fullName: "Rotterdam C.", code: "RTD"},
-        {fullName: "Nieuwerkerk", code: "NWK"},
-        {fullName: "Delft", code: "DT"}
+        {fullName: "RTD", code: "RTD"},
+        {fullName: "NWK", code: "NWK"},
+        {fullName: "DT", code: "DT"},
+        {fullname: "GD", code: "GD"},
+        {fullname: "SDM", code: "SDM"},
+        {fullname: "RTB", code: "RTB"},
+        {fullname: "RTA", code: "RTA"},
+        {fullname: "UT", code: "UT"},
+        {fullname: "DDR", code: "DDR"},
+        {fullname: "EHV", code: "EHV"},
+        {fullname: "GV", code: "GV"},
+        {fullname: "GVC", code: "GVC"},
+        {fullname: "LAA", code: "LAA"},
+
     ]
     return (
         <div className="station-container">
